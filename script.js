e0edceb42de9634b3728682867853086ed19992c
David___ <David.Stemmler@outlook.com>
2023-07-13 18:28:07 +0200

    Added-DropDownMenu

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-const dropdowns = document.querySelectorAll(".dropdown");
-dropdowns.forEach(dropdown =>{
-  const select = dropdown.querySelector(".select");
-  const caret = dropdown.querySelector(".caret")
-  const menu = dropdown.querySelector(".menu")
-  const options = dropdown.querySelector(".menu li")
-  const selected = dropdown.querySelector(".selected")
-  select.addEventListener("click", () =>{
-    select.classList.toggle("select-clicked")
-    caret.classList.toggle("caret-rotate")
-    menu.classList.toggle("menu-open")
-  })
-  options.forEach(option => {
-    option.addEventListener("click",() =>{
-      selected.innerText = option.innerText;
-      select.classList.remove("select-clicked")
-      caret.classList.remove("caret-rotate")
-      menu.classList.remove("menu-open")
-      options.forEach(option =>{
-        option.classList.remove("active")
-      })
-      option.classList.add("active")
-    })
-  })
-})
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
