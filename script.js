4d686d4355d8082d1237d74612e0952ad4765613
David___ <David.Stemmler@outlook.com>
2023-10-03 16:40:59 +0200

    DropDown changed

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+document.addEventListener("DOMContentLoaded", function() {
+  const dropdown = document.getElementById("myDropdown");
+  const dropbtn = document.querySelector('.dropbtn');
+  let isDropdownOpen = false;
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+  dropbtn.addEventListener("click", function(event) {
+    event.stopPropagation();
+
+    // Toggle the "show" class to open or close the dropdown
+    dropdown.classList.toggle("show");
+
+    // Toggle the "close-animation" class to initiate the closing animation
+    if (dropdown.classList.contains('show')) {
+      dropdown.style.maxHeight = dropdown.scrollHeight + "px";
+      isDropdownOpen = true;
+    } else {
+      dropdown.classList.add('close-animation');
+      dropdown.style.maxHeight = "0";
+      isDropdownOpen = false;
+
+      // Remove the "close-animation" class after the closing animation completes
+      setTimeout(function() {
+        dropdown.classList.remove('close-animation');
+      }, 500); // Adjust the time to match your CSS transition duration
+    }
+  });
+
+  // Close the dropdown when clicking outside
+  window.addEventListener("click", function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      if (isDropdownOpen) {
+        dropdown.classList.remove('show');
+        dropdown.style.maxHeight = "0";
+        isDropdownOpen = false;
       }
     }
-  }
-}
+  });
+});
